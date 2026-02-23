d412f2f2ec39a23217644e0db8bcac383187cbba
Dtatarchuk <dtatarchuk@swiftinvention.com>
2026-02-23 14:31:47 +0400

    copy files from itsmeefoundation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,77 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import starlight from "@astrojs/starlight";
-export const site = "https://smartwallet.mee.foundation/";
-export const sharedConfig = {
-  title: "Mee Smartwallet",
-  logo: {
-    light: "/src/assets/logo-light.svg",
-    dark: "/src/assets/logo-dark.svg",
-    replacesTitle: true,
-  },
-};
+export const site = "https://idagent.mee.foundation/";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  integrations: [tailwind()],
   site: site,
-  integrations: [
-    starlight({
-      ...sharedConfig,
-      disable404Route: true,
-      logo: {
-        light: "/src/assets/logo-light.svg",
-        dark: "/src/assets/logo-dark.svg",
-        replacesTitle: true,
-      },
-      editLink: {
-        baseUrl:
-          "https://github.com/meefoundation/smartwallet-website/edit/main/",
-      },
-      customCss: process.env.NO_GRADIENTS
-        ? []
-        : [
-            "/src/assets/landing.css",
-            "/src/styles/base.css",
-            "bootstrap-icons/font/bootstrap-icons.css",
-          ],
-      locales: {
-        root: { label: "English", lang: "en" },
-      },
-      sidebar: [
-        {
-          label: "Users",
-          items: [
-            {
-              label: "Common",
-              autogenerate: { directory: "docs/users/smartwallet" },
-            },
-          ],
-        },
-        {
-          label: "Developers",
-          items: [
-            {
-              label: "Common",
-              autogenerate: {
-                directory: "docs/developers/smartwallet",
-              },
-            },
-          ],
-        },
-      ],
-      components: {
-        Sidebar:
-          "./node_modules/mee-components/src/components/starlight/Sidebar.astro",
-        EditLink:
-          "./node_modules/mee-components/src/components/starlight/EditLink.astro",
-        PageFrame:
-          "./node_modules/mee-components/src/components/starlight/PageFrame.astro",
-        Footer:
-          "./node_modules/mee-components/src/components/starlight/Footer.astro",
-        Header: "./src/components/DocsHead.astro",
-      },
-    }),
-    tailwind(),
-  ],
 });
