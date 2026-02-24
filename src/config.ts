bd7b3e5ddd1ea3c6b01e69824ff4c90202b1cf80
DT <79907882+Dtatarchuk@users.noreply.github.com>
2026-02-24 17:14:35 +0400

    Update 24 02 26 (#4)
    * hide signin/signup links
    
    * update wordings
    
    * fix media queries

--- src/config.ts
+++ src/config.ts
@@ -1,82 +1,82 @@
 export const appLink = "https://app.itsmee.org/";
 export const meeFoundation = "https://mee.foundation/";
 export const github =
-  "https://github.com/MeeFoundation/private-advertising-website";
+	"https://github.com/MeeFoundation/private-advertising-website";
 export const discord = "https://discord.gg/PB8qxrQXup";
 export const header = {
-  logoLink: meeFoundation,
-  links: [],
-  rightLinks: [
-    {
-      name: "Join us",
-      link: discord,
-      icon: "discord",
-      ariaLabel: "Discord",
-      target: "_blank",
-      id: "discord",
-    },
-  ],
-  footerLinks: [
-    {
-      name: "About us",
-      link: `${meeFoundation}about/`,
-      id: "about",
-    },
-    {
-      link: `${meeFoundation}privacy-policy/`,
-      name: "Privacy",
-      id: "privacy-policy",
-    },
-  ],
-  authLinks: [
-    {
-      name: "Log in",
-      link: `${appLink}#sign-in`,
-      style: "outlined",
-      id: "sign-in",
-      buttonType: "primary",
-    },
-    {
-      name: "Sign up",
-      link: `${appLink}#sign-up`,
-      style: "filled",
-      id: "sign-up",
-      buttonType: "primary",
-      className: "hidden md:flex",
-    },
-  ],
+	logoLink: meeFoundation,
+	links: [],
+	rightLinks: [
+		{
+			name: "Join us",
+			link: discord,
+			icon: "discord",
+			ariaLabel: "Discord",
+			target: "_blank",
+			id: "discord",
+		},
+	],
+	footerLinks: [
+		{
+			name: "About us",
+			link: `${meeFoundation}about/`,
+			id: "about",
+		},
+		{
+			link: `${meeFoundation}privacy-policy/`,
+			name: "Privacy",
+			id: "privacy-policy",
+		},
+	],
+	// authLinks: [
+	// 	{
+	// 		name: "Log in",
+	// 		link: `${appLink}#sign-in`,
+	// 		style: "outlined",
+	// 		id: "sign-in",
+	// 		buttonType: "primary",
+	// 	},
+	// 	{
+	// 		name: "Sign up",
+	// 		link: `${appLink}#sign-up`,
+	// 		style: "filled",
+	// 		id: "sign-up",
+	// 		buttonType: "primary",
+	// 		className: "hidden md:flex",
+	// 	},
+	// ],
 };
 export const footer = {
-  links: [
-    {
-      link: "https://x.com/mee_foundation",
-      icon: "twitter-x",
-      target: "_blank",
-      ariaLabel: "X",
-    },
-    {
-      link: github,
-      icon: "github",
-      target: "_blank",
-      ariaLabel: "Github",
-    },
-    {
-      link: discord,
-      icon: "discord",
-      target: "_blank",
-      ariaLabel: "discord",
-    },
-  ],
-  rightLinks: [
-    {
-      name: "About us",
-      link: `${meeFoundation}about/`,
-      id: "about",
-    },
-    {
-      link: `${meeFoundation}privacy-policy/`,
-      name: "Privacy",
-      id: "privacy-policy",
-    },
-  ],
+	links: [
+		{
+			link: "https://x.com/mee_foundation",
+			icon: "twitter-x",
+			target: "_blank",
+			ariaLabel: "X",
+		},
+		{
+			link: github,
+			icon: "github",
+			target: "_blank",
+			ariaLabel: "Github",
+		},
+		{
+			link: discord,
+			icon: "discord",
+			target: "_blank",
+			ariaLabel: "discord",
+		},
+	],
+	rightLinks: [
+		{
+			name: "About us",
+			link: `${meeFoundation}about/`,
+			id: "about",
+		},
+		{
+			link: `${meeFoundation}privacy-policy/`,
+			name: "Privacy",
+			id: "privacy-policy",
+		},
+	],
 };
